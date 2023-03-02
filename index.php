c109d0acd552d59df96b0a1ce57ec8175a6f1897
Edu0103 <99186559+Edu0103@users.noreply.github.com>
2023-03-02 17:45:43 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro php</title>
+   </head>
+<body>
+   <h1> <?php echo "Ola Mundo" ?> </h1>
+   <h2> <?php echo "Soma +" ?> </h2>
+   <h2> <?php echo "Subtração - " ?> </h2>
+   <h2> <?php echo "Multiplicação / " ?> </h2>
+   <h2> <?php echo "Divisão *" ?> </h2>
+</body>
+   </html>
